cb6796045c41e4f8f946df81b49125d9812d223a
Your Name <you@example.com>
2017-07-24 10:31:08 +0800

    add a pcPorduct page

--- src/component/pcNewsContainer.js
+++ src/component/pcNewsContainer.js
@@ -4,6 +4,7 @@ import {Row, Col} from 'antd';
 import {Tabs, Carousel} from 'antd';
 import PCNewsBlock from './pcNewsBlock';
 import PCNewsImageBlock from './pcNewsImageBlock.js';
+import PCProduct from './pcProdect.js';
 
 const TabPane = Tabs.TabPane;
 
@@ -30,27 +31,42 @@ export default class PCNewsContainer extends React.Component {
                 </Carousel>
               </div>
 
+              <PCNewsImageBlock count = {3} type = "top" width = "100%" cardTitle = {"头条"} imageWidth = "112px"></PCNewsImageBlock>
+
             </div>
             <Tabs className = "tabs_news">
               <TabPane tab="头条" key="1">
-                <PCNewsBlock count ={10} type= "top" width="100%" bordered="false"/>
+                <PCNewsBlock count ={15} type= "top" width="100%" bordered="false"/>
               </TabPane>
 
               <TabPane tab="国际" key="2">
-                <PCNewsBlock count ={10} type= "guoji" width="100%" bordered="false"/>
+                <PCNewsBlock count ={15} type= "guoji" width="100%" bordered="false"/>
               </TabPane>
 
               <TabPane tab="国内" key="3">
-                <PCNewsBlock count ={10} type= "guonei" width="100%" bordered="false"/>
+                <PCNewsBlock count ={15} type= "guonei" width="100%" bordered="false"/>
               </TabPane>
 
               <TabPane tab="社会" key="4">
-                <PCNewsBlock count ={10} type= "shehui" width="100%" bordered="false"/>
+                <PCNewsBlock count ={15} type= "shehui" width="100%" bordered="false"/>
               </TabPane>
 
             </Tabs>
-            <PCNewsImageBlock count = {8} type = "top" width = "100%" cardTitle = {"头条"} imageWidth = "112px"></PCNewsImageBlock>
-              <PCNewsImageBlock count = {8} type = "yule" width = "100%" cardTitle = {"娱乐"} imageWidth = "112px"></PCNewsImageBlock>
+            <Tabs className = "tabs_product">
+              <TabPane tab = "ReactNews" key = "1">
+                <PCProduct></PCProduct>
+              </TabPane>
+              <TabPane tab = "ReactNews" key = "2">
+                <PCProduct></PCProduct>
+              </TabPane>
+              <TabPane tab = "ReactNews" key = "3">
+                <PCProduct></PCProduct>
+              </TabPane>
+            </Tabs>
+
+
+            <PCNewsImageBlock count = {9} type = "top" width = "100%" cardTitle = {"头条"} imageWidth = "112px"></PCNewsImageBlock>
+            <PCNewsImageBlock count = {9} type = "yule" width = "100%" cardTitle = {"娱乐"} imageWidth = "112px"></PCNewsImageBlock>
 
           </Col>
           <Col span={2}></Col>
